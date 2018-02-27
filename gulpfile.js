523f4b20f412b11c9dba494d7dd17a34fd25e7d2
Christian Medina <medinachristian1984@gmail.com>
2018-02-27 18:44:37 -0400

    Upload files

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const plumber = require('gulp-plumber');
+const server = require('browser-sync').create();
+
+const postCCSPlugins = [
+  cssnano({
+    core: false,
+    autoprefixer: {
+      add: true,
+      browsers: 'last 2 versions'
+    }
+  })
+];
+
+gulp.task('pug', () => {
+  gulp.src('./dev/pug/pages/*.pug')
+    .pipe(plumber())
+    .pipe(pug({ pretty: true }))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('sass', () => {
+  gulp.src('./dev/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss(postCCSPlugins))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(server.stream({ match: '**/*.css' }));
+});
+
+gulp.task('default', ['pug', 'sass'], () => {
+  server.init({ server: './public' });
+})
+
+gulp.watch('./dev/pug/**/*.pug', ['pug', server.reload]);
+gulp.watch('./dev/scss/**/*.scss', ['sass']);
